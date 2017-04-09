2a784d193fd612ae0ee19cb6a0c34c198c2196e6
Sridhar Mocherla <srmocher@email.arizona.edu>
2017-04-09 15:10:33 -0700

    btreefile added constructors, destructor and implemented partial insert

--- proj3/btree/src/btfile.C
+++ proj3/btree/src/btfile.C
@@ -5,6 +5,7 @@
  * Edited by Young-K. Suh (yksuh@cs.arizona.edu) 03/27/14 CS560 Database Systems Implementation 
  */
 
+#include <btfile.h>
 #include "minirel.h"
 #include "buf.h"
 #include "db.h"
@@ -39,10 +40,25 @@ const char* BtreeErrorMsgs[] = {
   // CANT_SPLIT_INDEX_PAGE
 };
 
+
+
+
+
 static error_string_table btree_table( BTREE, BtreeErrorMsgs);
 
 BTreeFile::BTreeFile (Status& returnStatus, const char *filename)
 {
+ // PageId headerPageId;
+  Status status = MINIBASE_DB->get_file_entry(filename,headerPageId);
+  if(status!=OK)
+  {
+    returnStatus=status;
+    return;
+  }
+  Page *pg;
+  returnStatus = MINIBASE_BM->pinPage(headerPageId,pg,0,filename);
+  headerPage = (HeaderPage *)pg;
+  this->fileName = filename;
   // put your code here
 }
 
@@ -50,22 +66,95 @@ BTreeFile::BTreeFile (Status& returnStatus, const char *filename,
                       const AttrType keytype,
                       const int keysize)
 {
+   // PageId headerPageId;
+    Status status = MINIBASE_DB->get_file_entry(filename,headerPageId);
+    this->fileName = filename;
+    if(status!=OK) //index doesn't exist
+    {
+        Page *hPage;
+        returnStatus = MINIBASE_BM->newPage(headerPageId,hPage,1);
+        headerPage = (HeaderPage *)hPage;
+        headerPage->keyType = keytype;
+        headerPage->keyLength = keysize;
+        headerPage->pageId = -1;
+        headerPage->numLevels = 0;
+        return;
+    }
+    else
+    {
+        Page *pg;
+        returnStatus = MINIBASE_BM->pinPage(headerPageId,pg,0,filename);
+        headerPage = (HeaderPage *)pg;
+    }
   // put your code here
 }
 
 BTreeFile::~BTreeFile ()
 {
+   Page *pg = (Page *)headerPage;
+   MINIBASE_BM->unpinPage(headerPageId,true,fileName.c_str());
   // put your code here
 }
 
 Status BTreeFile::destroyFile ()
 {
   // put your code here
-  return OK;
+  Status status= MINIBASE_DB->delete_file_entry(fileName.c_str());
+  return status;
 }
 
 Status BTreeFile::insert(const void *key, const RID rid) {
   // put your code here
+
+  PageId rootId = headerPage->pageId;
+
+  if(headerPage->pageId == -1)
+  {
+      Page *page;
+      PageId pgId;
+      MINIBASE_BM->newPage(pgId,page,1);
+      headerPage->pageId = pgId;
+      BTLeafPage *leaf = (BTLeafPage *)page;
+
+      RID leafID;
+      Status status = leaf->insertRec(key,headerPage->keyType,rid,leafID);
+      MINIBASE_BM->unpinPage(pgId,TRUE,fileName.c_str());
+      return status;
+  }
+  else
+  {
+      Page *rootPage;
+      MINIBASE_BM->pinPage(headerPage->pageId,rootPage,false);
+      if(headerPage->numLevels==0)
+      {
+          BTLeafPage *page = (BTLeafPage *)rootPage;
+          int recLen;
+          if(headerPage->keyType == attrInteger)
+              recLen = sizeof(int) + sizeof(RID);
+          else if(headerPage->keyType == attrString)
+          {
+              char *record = (char *)key;
+              recLen = strlen(record) + sizeof(RID);
+          }
+          RID entryId;
+          if(page->available_space()>recLen)
+          {
+             Status status =  page->insertRec(key,headerPage->keyType,rid,entryId);
+             MINIBASE_BM->unpinPage(headerPage->pageId,true,fileName.c_str());
+             return status;
+          }
+          else //split root page
+          {
+              BTIndexPage *page;
+
+          }
+      }
+      else
+      {
+
+      }
+
+  }
   return OK;
 }
 
