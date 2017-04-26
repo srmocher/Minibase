10531dfe1c4df49c47abf3fabd2a80f3eae6a0ae
Sridhar Mocherla <srmocher@email.arizona.edu>
2017-04-26 14:31:53 -0700

    Added code for sorting files and scan (partially)

--- proj4/SortMerge/src/sortMerge.C
+++ proj4/SortMerge/src/sortMerge.C
@@ -36,6 +36,58 @@ sortMerge::sortMerge(
     Status&         s               // Status of constructor
 ){
 	// fill in the body
+    Status status;
+
+
+    Sort sortFirst(filename1,"firstFile",len_in1,in1,t1_str_sizes,join_col_in1,order,amt_of_mem,status);
+    Sort sortSecond(filename2,"secondFile",len_in2,in2,t2_str_sizes,join_col_in2,order,amt_of_mem,status);
+
+    firstFile = new HeapFile("firstFile",status);
+
+    secondFile = new HeapFile("secondFile",status);
+    mergedFile = new HeapFile(filename3,status);
+
+    Scan *first = firstFile->openScan(status);
+    Scan *second = secondFile->openScan(status);
+    AttrType firstAttr = in1[join_col_in1];
+    AttrType  secondAttr = in2[join_col_in2];
+
+    int firstSize = t1_str_sizes[join_col_in1];
+    int secondSize = t2_str_sizes[join_col_in2];
+    if(firstAttr!=secondAttr) {
+        s = DONE;
+        return;
+    }
+    RID firstRID,secondRID;
+    char *firstRecord, *secondRecord;
+    int firstLength,secondLength;
+
+    int firstOffset = 0,secondOffset = 0;
+    int i=0;
+    while(i < join_col_in1)
+    {
+        firstOffset += t1_str_sizes[i];
+        i++;
+    }
+    char *joinFirstColumn = new char[t1_str_sizes[i]];
+    i=0;
+    while (i < join_col_in2)
+    {
+        secondOffset += t2_str_sizes[i];
+        i++;
+    }
+    char *joinSecondColumn = new char[t2_str_sizes[i]];
+    while(first->getNext(firstRID,firstRecord,firstLength) == OK && second->getNext(secondRID,secondRecord,secondLength)==OK)
+    {
+        memcpy(joinFirstColumn,firstRecord+firstOffset,t1_str_sizes[join_col_in1]);
+        memcpy(joinSecondColumn,secondRecord+secondOffset,t2_str_sizes[join_col_in2]);
+        if(tupleCmp(joinFirstColumn,joinSecondColumn)==0)
+        {
+
+        }
+    }
+
+
 }
 
 // sortMerge destructor
